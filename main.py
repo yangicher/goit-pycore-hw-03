8e88dec833a83a74cb268f5e9c219eb8f5281bc6
Serge Iangicher <sergey.yangicher@rockyroad.io>
2024-10-07 12:32:08 +0300

    homework 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import random
 
 def get_days_from_today(date : str) -> int:
     'string, format -> new datetime parsed from a string ("YYYY-MM-DD").'
@@ -8,4 +9,18 @@ def get_days_from_today(date : str) -> int:
     except ValueError as e: 
         print('ValueError Raised:', e) 
 
+def get_numbers_ticket(min : int, max : int, quantity : int):
+    """min -> positive number, 
+       max -> less than 1000 or equal,
+       quantity -> result amount"""
+    if(min < 1 or max > 1000):
+        return []
+    elif(max + 1 - min >= quantity):
+        numbers = [i for i in range(min, max + 1)]
+        chosen_numbers = random.sample(numbers, quantity)
+        return sorted(chosen_numbers)
+    return []
+
 print(get_days_from_today("2025-10-05"))
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
