a053b269b47c9cab1a2c69348c15b4c47a41b56b
Serge Iangicher <sergey.yangicher@rockyroad.io>
2024-10-07 15:36:10 +0300

    support upcoming_period

--- hw4.py
+++ hw4.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+upcoming_period = 7
 days_in_week = 7
 
 def get_upcoming_birthdays(users):
@@ -13,20 +14,21 @@ def get_upcoming_birthdays(users):
         if (birthday_this_year < today.date()):
             birthday_this_year = user_birthday.date().replace(year=today.year + 1)
 
-        weekday = birthday_this_year.weekday()
-        if (weekday == 6 or weekday == 5):
-            begin_week = birthday_this_year + timedelta(days = days_in_week - weekday)
-            upcoming_birthdays.append(f"name: {user["name"]}, congratulation_date: {begin_week.strftime("%Y.%m.%d")}")
-        else:
-            upcoming_birthdays.append(f"name: {user["name"]}, congratulation_date: {birthday_this_year.strftime("%Y.%m.%d")}")
-            
+        diff = (birthday_this_year - today.date()).days
+
+        if(diff >= 0 and diff <= upcoming_period): 
+            weekday = birthday_this_year.weekday()
+            if(weekday == 6 or weekday == 5):
+                begin_week = birthday_this_year + timedelta(days = days_in_week - weekday)
+                upcoming_birthdays.append(f"name: {user["name"]}, congratulation_date: {begin_week.strftime("%Y.%m.%d")}")
+            else:
+                upcoming_birthdays.append(f"name: {user["name"]}, congratulation_date: {birthday_this_year.strftime("%Y.%m.%d")}")
     return upcoming_birthdays
 
 users = [
-    {"name": "John Doe", "birthday": "1985.10.05"},
+    {"name": "John Doe", "birthday": "1985.10.08"},
     {"name": "Jane Smith", "birthday": "1990.03.14"}
 ]
 
 upcoming_birthdays = get_upcoming_birthdays(users)
-
 print("Список привітань на цьому тижні:", upcoming_birthdays)
